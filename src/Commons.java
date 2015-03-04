0a6c254ae8808900d4c93ec8fcd39d33830dcf30
rafahinojosa <sonicv411@hotmail.com>
2015-03-04 17:59:34 -0600

    En procesoasdfasdfasdf

--- src/Commons.java
+++ src/Commons.java
@@ -13,7 +13,8 @@ public interface Commons {
     public final int BOARD_WIDTH = 716;
     public final int BOARD_HEIGTH = 700;
     public static final int GROUND = 580;
-    public static final int BOMB_HEIGHT = 5;
+    public static final int BOMB_HEIGHT = 15;
+    public static final int BOMB_WIDTH = 15;
     public static final int ALIEN_HEIGHT = 12;
     public static final int ALIEN_WIDTH = 12;
     public static final int BORDER_RIGHT = 30;
@@ -22,6 +23,6 @@ public interface Commons {
     public static final int NUMBER_OF_ALIENS_TO_DESTROY = 24;
     public static final int CHANCE = 5;
     public static final int DELAY = 17;
-    public static final int PLAYER_WIDTH = 15;
-    public static final int PLAYER_HEIGHT = 10;
+    public static final int PLAYER_WIDTH = 50;
+    public static final int PLAYER_HEIGHT = 50;
 }
